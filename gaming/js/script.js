94237cd00a1e1f6db0a5ebfeca1800c909584459
Ben <bencollins2@gmail.com>
2012-10-10 11:54:33 -0400

    Noon on Wednesday

--- gaming/js/script.js
+++ gaming/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	//$(".transparent").hide();
-	$("#li1").mouseenter(function(){mouseHover("#a1", "in")});
+	/*$("#li1").mouseenter(function(){mouseHover("#a1", "in")});
 	$("#li2").mouseenter(function(){mouseHover("#a2", "in")});
 	$("#li3").mouseenter(function(){mouseHover("#a3", "in")});
 	$("#li4").mouseenter(function(){mouseHover("#a4", "in")});
@@ -9,7 +9,7 @@ $(document).ready(function(){
 	$("#li2").mouseleave(function(){mouseHover("#a2", "out")});
 	$("#li3").mouseleave(function(){mouseHover("#a3", "out")});
 	$("#li4").mouseleave(function(){mouseHover("#a4", "out")});
-	$("#li5").mouseleave(function(){mouseHover("#a5", "out")});
+	$("#li5").mouseleave(function(){mouseHover("#a5", "out")});*/
 });
 
 // Display the target when mouse hover
