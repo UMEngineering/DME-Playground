8208e84c73e309db911e7985c05723d67a72ae95
Haoyi Yang <yanghy89632@gmail.com>
2012-10-24 14:20:33 -0400

    Add YUI scrollview

--- decision/js/main.js
+++ decision/js/main.js
@@ -1,1 +1,12 @@
 
+function changePage(page){
+	var nextSteps = new Array(
+		["NEXT STEPS", ["img/scroll/next/step1.png", "Checklist of admissions tasks"], ["img/scroll/next/step2.png", "Register for Campus Day"], ["img/scroll/next/step3.png", "Put dates onto your Google Cal"]], 
+		["MEET SOME STUDENTS", ["img/scroll/next/stud1.png", "Meet Hannah Cheriyan"], ["img/scroll/next/stud2.png", "Meet John Qiao"], ["img/scroll/next/stud3.png", "Meet Ashley Godin"]], 
+		["EXPLORE ANN ARBOR", ["img/scroll/next/expl1.png", "SEE students at their fav spots"], ["img/scroll/next/expl2.png", "Watch video about A2's spirit"], ["img/scroll/next/expl3.png", "Explore a map of North Campus"]], 
+		["MAKE CONNECTIONS", ["img/scroll/next/conn1.png", "Declare your choice to others"], ["img/scroll/next/conn2.png", "Join us on Twitter or Facebook"], ["img/scroll/next/conn3.png", "Email an advisor"]]);
+	var explore = new Array(
+		["ACADEMICS", ["img/scroll/expl/acad1.png", "Undergrad Research"], ["img/scroll/expl/acad2.png", "Tutoring"], ["img/scroll/expl/acad3.png", "Honors Program"]], 
+		["OPPORTUNITIES", ["img/scroll/expl/oppo1.png", "Student Teams &amp; Organizations"], ["img/scroll/expl/oppo2.png", "Volunteering &amp; Service"], ["img/scroll/expl/oppo3.png", "Multidisciplinary Design Program"]], 
+		["LIFE &amp; ACTIVITIES", ["img/scroll/expl/life1.png", "Arts &amp; Culture"], ["img/scroll/expl/life2.png", "Greek Life"], ["img/scroll/expl/life3.png", "Athletics"]]);
+}
