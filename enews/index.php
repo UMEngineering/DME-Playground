1280ab5e1da4e50d85a67b59077368ca8ce7639f
Ben <bencollins2@gmail.com>
2013-01-30 11:18:47 -0500

    11:18

--- enews/index.php
+++ enews/index.php
@@ -44,6 +44,7 @@ if (!$result){
 	
 	
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+
     <script>window.jQuery || document.write('<script src="js/jquery-1.8.2.min.js"><\/script>')</script>
 </body>
 </html>
