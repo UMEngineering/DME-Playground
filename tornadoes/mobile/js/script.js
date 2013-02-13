cfbda599cb0bad2ae3aaac21ac9cebf1a7dba125
Haoyi Yang <yanghy89632@gmail.com>
2013-02-13 16:52:39 -0500

    working for drones mobile (drones2)

--- tornadoes/mobile/js/script.js
+++ tornadoes/mobile/js/script.js
@@ -11,14 +11,14 @@ $(document).ready(function() {
 	$("iframe").attr("height", iframe_height);
 	
 	// Set the width of different elements of the page
-	$("section").css("width", width);
-	$("#footer").css("width", width);
-	$(".sticky").css("width", width);
-	$("#two img").css("width", width);
-	$("#three img").css("width", width);
-	$("#six img").css("width", width);
+	$("section").css("width", width + "px");
+	$("#footer").css("width", width + "px");
+	$(".sticky").css("width", width + "px");
+	$("#two img").css("width", width + "px");
+	$("#three img").css("width", width + "px");
+	$("#six img").css("width", width + "px");
 	
 	if (width < 380){
-		$("#nav img").css("width", width);
+		$("#nav img").css("width", width + "px");
 	}
 });
