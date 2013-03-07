8fcae9f0b266ec69654d3ad5fc9d0a3adc5a6a50
Ben <bencollins2@gmail.com>
2013-03-07 14:18:10 -0500

    2:17
    Not sure why it says "ethics_design" is new, but I'm renaming it to
    avoid a conflict.

--- profiles-portlet-update/profiles-script.js
+++ profiles-portlet-update/profiles-script.js
@@ -1,1 +1,1 @@
-$("div#content div.promoHolderProfiles span.profilesMichEng").fadeTo(3500,0.75).fadeOut(1500);
+$("div#contentWrap div.promoHolderProfiles span.profilesMichEng").fadeTo(3500,0.75).fadeOut(1500);
---
+++ tabs.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+	if ($(document.body).hasClass("portaltype-uom-pfiles-profile")) {
+		alert("test");
+	  $("#tabs").profiletabs({ labelledBy: "tabsLbl" });
+	  // Adds profile tabs and ARIA for tabs
+	}
+})
+
