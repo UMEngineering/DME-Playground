8fc3d0d0644f622630cd7a86f87441fc2186cdf8
Haoyi Yang <yanghy89632@gmail.com>
2013-05-02 11:50:26 -0400

    11:50 some small fixes

--- campaign/js/main.js
+++ campaign/js/main.js
@@ -15,6 +15,7 @@ $(document).ready(function(e) {
     $(".img-cover").each(function(index, element) {
         var item_id = $(this).parent().attr("id");
 		
+		/* Click event: when click on an image, load the image and append it into the HTML */
 		$(this).click(function(e) {
 			current = item_id;
 			
@@ -24,9 +25,10 @@ $(document).ready(function(e) {
 			
 			// Clear all content in all items
 			$("#" + item_id + " .img-cover").css({"display" : "none"});
-			$("#" + item_id + " .item-content").html('<img style="height: ' + height + 'px;" src="img/big/' + item_id_img + '_big.jpg" alt="item image" />');
+			$("#" + item_id + " .item-content").html('<img class="content-image" style="height: ' + height + 'px;" src="img/big/' + item_id_img + '_big.jpg" alt="item image" />');
 			$("#go-back").show();
 
+			// Expand the selected item to full screen, and make all other cover images with as 0 so they will disappear
 			$(".one-item").each(function(index, element) {
                 if ($(this).attr("id") != item_id) {
 					$(this).css({"width" : "0px"});
@@ -41,20 +43,24 @@ $(document).ready(function(e) {
 	$("#go-back").click(function(e) {
 		if (current != "") {
 			console.log(current + " " + $("#" + current).width());
-			var added = $("#" + current).width() + 180;
+			var added = $("#" + current).width() + 200;
 			console.log(added);
 			$("#" + current).css({"width" : added + "px"});
-			console.log($("#" + current).css("width"));
+			console.log($("#" + current).width());
 		}
+		
+		/*$(".content-image").css({"position" : "absolute", "top" : "0", "left" : "0"});*/
+		$("#" + current).css({"white-space" : "nowrap"});
 		
 		$(".one-item").each(function(index, element) {
 			$(this).css({"width" : "180px"});
 		});
 		
-		$(".img-cover").css({"display" : "block"});
 		setTimeout(function(){
+			//$(".img-cover").css({"display" : "block"});
 			$(".item-content").html("");
 			$("#go-back").hide();
+			//$(".img-cover").fadeIn(500);
 		}, 500);
     });
 });
